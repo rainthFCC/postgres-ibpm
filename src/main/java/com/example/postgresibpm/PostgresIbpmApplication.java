22d7d68ea34eff36c6c3254b550236babe821878
Arturo Garcia <arturogarcia.mad@gmail.com>
2019-09-18 22:24:28 -0500

    First Commit
    Signed-off-by: Arturo Garcia <arturogarcia.mad@gmail.com>

---
+++ src/main/java/com/example/postgresibpm/PostgresIbpmApplication.java
@@ -0,0 +1,13 @@
+package com.example.postgresibpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresIbpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresIbpmApplication.class, args);
+	}
+
+}
